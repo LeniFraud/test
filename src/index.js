c1034764d7945cdc57a081f6d28b6630ab44b5d7
Anastasiia Klyosova <112250768+LeniFraud@users.noreply.github.com>
2023-03-07 18:18:31 +0200

    index.js fix

--- src/index.js
+++ src/index.js
@@ -13,9 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter
-        // basename="/test/"
-        >
+        <BrowserRouter basename="/test/">
           <ThemeProvider theme={theme}>
             <GlobalStyle />
             <App />
